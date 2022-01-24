5bfaded69a59fe533370d8d5dae912a0910c958b
rndjhds <86414343+rndjhds@users.noreply.github.com>
2022-01-24 17:35:32 +0900

    complete homework 2022_01_24

--- homework.sql
+++ homework.sql
@@ -13,3 +13,10 @@ select nvl(to_char(mgr), 'CEO') mgr from emp;
 -- to_char는 숫자를 문자로 형변환 해주는 함수이다.
 
 select * from emp;
+
+--     Q1. 사원 테이블(EMP)에서 가장 최근에 입사한 사원명을 출력 하는 
+--           SQL문을 작성 하세요?
+select ename from emp where hiredate = (select max(hiredate) from emp);
+--     Q2. 사원 테이블(EMP)에서 최대 급여를 받는 사원명과 최대급여
+--           금액을 출력하는 SQL문을 작성 하세요?
+select ename, sal from emp where sal = (select max(sal) from emp);
